ae7453e733379b469d546dc1b25a05e353336d52
cvgs9210 <cavanegas9210@gmail.com>
2022-07-28 23:18:16 -0500

    Started project.

---
+++ alien_invasion.py
@@ -0,0 +1,24 @@
+import sys
+
+import pygame
+
+class AlienInvasion:
+    """Clase general para gestionar los recursos y el comportammiento del juego."""
+
+    def __init__(self):
+        """Inicializa el juego y crea recursos."""
+        pygame.init()
+        
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Inicia el bucle principal para el juego"""
+        while True:
+            #Busca eventos del teclado y el raton
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            #Hace visible la ultima pantalla dibujada.
+            pygame.display.flip()
+            
