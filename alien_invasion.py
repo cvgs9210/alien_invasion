bb55f8ec3fb9f0e433efcce4e3292a5975ae4dd1
cvgs9210 <cavanegas9210@gmail.com>
2022-07-28 23:37:40 -0500

    AlienInvacion_V1.0.0

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,18 +2,22 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Clase general para gestionar los recursos y el comportammiento del juego."""
 
     def __init__(self):
         """Inicializa el juego y crea recursos."""
         pygame.init()
+        self.settings = Settings()
         
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         #Configura el color del fondo.
-        self.bg_color = (230, 230, 230)
+        #self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Inicia el bucle principal para el juego"""
@@ -24,10 +28,11 @@ class AlienInvasion:
                     sys.exit()
 
             #Redibuja la pantalla en cada paso por le bucle
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             
             #Hace visible la ultima pantalla dibujada.
             pygame.display.flip()
+
 if __name__ == '__main__':
     #Hace una instancia del juego y lo ejecuta.
     ai = AlienInvasion()
