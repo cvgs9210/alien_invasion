6417889e3a96b5c8c10465966e8ee17bc489487a
cvgs9210 <cavanegas9210@gmail.com>
2022-07-30 23:37:41 -0500

    AlienInvasion_V.1.0.2

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 import sys
+from time import sleep
 import pygame
 
 from settings import Settings
@@ -13,7 +14,11 @@ class AlienInvasion:
         self.settings = Settings()
         
         self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+           (self.settings.screen_width, self.settings.screen_height))
+        """PANTALLA COMPLETA
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height"""
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
@@ -24,17 +29,45 @@ class AlienInvasion:
     def run_game(self):
         """Inicia el bucle principal para el juego"""
         while True:
-            #Busca eventos del teclado y el raton
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
 
-            #Redibuja la pantalla en cada paso por le bucle
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            
-            #Hace visible la ultima pantalla dibujada.
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+    def _check_events(self):
+        #Busca eventos del teclado y el raton
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+    
+    def _check_keydown_events(self, event):
+        """Responde a pulsaciones de teclas"""
+        if event.key == pygame.K_RIGHT:
+            #Mueve la nave a la derecha
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """Responde a las liberaciones de las teclas"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _update_screen(self):
+        #Redibuja la pantalla en cada paso por le bucle
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        
+        #Hace visible la ultima pantalla dibujada.
+        pygame.display.flip()
 
 if __name__ == '__main__':
     #Hace una instancia del juego y lo ejecuta.
--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         """Inicializa la nave y configura su posicion inicial."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         #Carga la imagen de la nave y obtiene su rect.
@@ -14,6 +15,24 @@ class Ship:
 
         #Coloca inicialmente cada nave nueva en le centro de la parte inferior de la pantalla
         self.rect.midbottom = self.screen_rect.midbottom
+
+        #Guarda un valor decimal para la posición horizontal de la nave.
+        self.x = float(self.rect.x)
+
+        #Bandera de movimiento
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Actualiza la posición de la nave en función de la bandera de movimiento."""
+        #Actualiza el valor de x de la nave, no el rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        #Actualiza el objeto rect de self.x
+        self.rect.x = self.x
     
     def blitme(self):
         """Dibuja la nave en su ubicacion actual"""
