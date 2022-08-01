93b0ee41450b908ae22da0d44008d8b3e86334a2
cvgs9210 <cavanegas9210@gmail.com>
2022-07-31 23:49:01 -0500

    AlienInvasion_V1.1.4

--- alien_invasion.py
+++ alien_invasion.py
@@ -82,6 +82,7 @@ class AlienInvasion:
         self.stats.game_active = True
         self.sb.prep_score()
         self.sb.prep_level()
+        self.sb.prep_ships()
 
         # Se deshace de los aliens y balas restantes.
         self.aliens.empty()
@@ -249,6 +250,7 @@ class AlienInvasion:
 
             # Disminuye ships_left.
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Se deshace de los aliens y balas restantes.
             self.aliens.empty()
--- scoreboard.py
+++ scoreboard.py
@@ -1,10 +1,14 @@
 import pygame.font
+from pygame.sprite import Group
+
+from ship import Ship
  
 class Scoreboard:
     """Una clase para dar informacion de la puntuacion"""
 
     def __init__(self, ai_game):
         """Inicializa los atributos de la puntuacion."""
+        self.ai_game = ai_game
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
@@ -18,6 +22,16 @@ class Scoreboard:
         self.prep_score()
         self.prep_high_score()
         self.prep_level()
+        self.prep_ships()
+
+    def prep_ships(self):
+        """Muestra cuantas naves quedan"""
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_game)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
 
     def prep_score(self):
         """Convierte la puntuacion en una imagen renderizada."""
@@ -43,6 +57,7 @@ class Scoreboard:
         self.screen.blit(self.level_tag, self.level_tag_rect)
         self.screen.blit(self.score_tag, self.score_tag_rect)
         self.screen.blit(self.high_score_tag, self.high_score_tag_rect)
+        self.ships.draw(self.screen)
 
     def prep_high_score(self):
         """Convierte la puntuacion mas alta en una imagen renderizada."""
